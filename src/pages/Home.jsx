5ab01e2e3d361faad8cda0d376d4eeea353dc4b0
Eric9516 <eric-995-@hotmail.com>
2023-03-09 20:36:46 -0300

    modificaciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,68 +1,66 @@
-import { useEffect, useState } from "react";
-
-import { getCountries } from "../services/countries";
-import { getCities } from "../services/cities";
-import { getCityWeather } from "../services/weather";
+import React, { useEffect, useState } from "react";
+import Icons from "../components/Icons";
 
 export const Home = () => {
-	const [countries, setCountries] = useState([]);
-	const [cities, setCities] = useState([]);
-	const [weather, setWeather] = useState(null);
+	const [search, setSearch] = useState("");
+	const [values, setValues] = useState("");
+	const [icon, setIcons] = useState("");
 
-	useEffect(() => {
-		(async () => {
-			setCountries(await getCountries());
-		})();
-	}, []);
+	const URL = `http://api.openweathermap.org/data/2.5/weather?q=${search}&lang=ES&units=metric&appid=3eaea1096fcf29eaf3c1e7217f06cdcc`;
 
-	const countryHandler = async (e) => {
-		e.currentTarget.value && setCities(await getCities(e.currentTarget.value));
-		setWeather(null);
+	const getData = async () => {
+		await fetch(URL)
+			.then((res) => res.json())
+			.then((res) => {
+				if (res.cod >= 400) {
+					return setValues(false);
+				} else {
+					console.log(res.weather[0].main);
+					setIcons(res.weather[0].main);
+					setValues(res);
+				}
+			})
+			.catch((err) => console.log(err));
 	};
 
-	const cityHandler = async (e) =>
-		e.currentTarget.value &&
-		setWeather(await getCityWeather(e.currentTarget.value));
+	useEffect(() => {
+		getData();
+	}, [search]);
+
+	const handleSearch = (e) => {
+		if (e.key === "Enter") {
+			setSearch(e.target.value);
+		}
+	};
 
 	return (
 		<>
 			<div>
-				<label>Elige un país:</label>
-				<select onChange={countryHandler}>
-					<option value="">Selecciona</option>
-					{countries.map((country) => (
-						<option key={country.cca2} value={country.cca2}>
-							{country.name.common}
-						</option>
-					))}
-				</select>
-			</div>
-
-			{cities.length > 0 && (
+				<h1>Weather App</h1>
 				<div>
-					<label>Elige una ciudad:</label>
-					<select onChange={cityHandler}>
-						<option value="">Selecciona</option>
-						{cities.map((city) => (
-							<option key={city.id}>{city.name}</option>
-						))}
-					</select>
-				</div>
-			)}
-
-			<hr />
-
-			{weather && (
-				<div>
-					<h2>Actual temperature: {weather.main.temp}º</h2>
-					<p>Min: {weather.main.temp_min.toFixed()}°</p>
-					<p>Max: {weather.main.temp_max.toFixed()}°</p>
-					<img
-						src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-						alt="weather icon"
+					<input
+						placeholder="Buscar ciudad"
+						onKeyDown={handleSearch}
+						autoFocus
 					/>
 				</div>
-			)}
+			</div>
+
+			<div>
+				{values ? (
+					<div>
+						<h3>{values.name}</h3>
+						<p>{values.main.temp.toFixed(0)}&deg;</p>
+						<p>
+							{values.main.temp_min.toFixed(0)}&deg; |
+							{values.main.temp_max.toFixed(0)}&deg;
+						</p>
+						<img src={Icons(icon)} alt="icono" />
+					</div>
+				) : (
+					<h1>Ciudad no encontrada</h1>
+				)}
+			</div>
 		</>
 	);
 };
